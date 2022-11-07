12e87411cdcc7853d3a28aa8ff01f25094247397
Shrouq <shrouq.talhi@gmail.com>
2022-11-07 13:14:44 +0300

    Create Events Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
+import Events from "./components/Events";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -11,6 +12,8 @@ function App() {
         <Route path="/"></Route>
         <Route path="/SignUp" element={<SignUp />}></Route>
         <Route path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/Events" element={<Events />}></Route>
+
       </Routes>
 
     </div>
---
+++ src/components/Events.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function Events() {
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="imgBx">
+            <img src="https://i.pinimg.com/564x/8f/9f/75/8f9f7586b48e1dd8f22de0f91c7c308e.jpg"></img>
+          </div>
+          <div className="content">
+            <h1> kids Events</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+              officiis. Maxime odit eos placeat labore non odio quaerat quod,
+              nihil, quidem ut cum earum laboriosam, .
+            </p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBx">
+            <img src="https://i.pinimg.com/564x/8f/9f/75/8f9f7586b48e1dd8f22de0f91c7c308e.jpg"></img>
+          </div>
+          <div className="content">
+            <h1> kids Events</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+              officiis. Maxime odit eos placeat labore non odio quaerat quod,
+              nihil, quidem ut cum earum laboriosam, .
+            </p>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="imgBx">
+            <img src="https://i.pinimg.com/564x/8f/9f/75/8f9f7586b48e1dd8f22de0f91c7c308e.jpg"></img>
+          </div>
+          <div className="content">
+            <h1> kids Events</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+              officiis. Maxime odit eos placeat labore non odio quaerat quod,
+              nihil, quidem ut cum earum laboriosam,.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Events;
