a0336d2b59513a8210c4388eb3130e356f5baaf5
Areej <Areej-fahad>
2022-11-07 07:46:59 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Component/Navbar';
+// import Contact from './Component/Contact';
+// import Home from './Component/Home';
+// import Footer from "./Component/Footer"
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+        {/* <Home></Home>    */}
+        {/* <Contact></Contact>    */}
+        {/* <Footer></Footer> */}
+
+
+    </div>
+  );
+}
+
+export default App;
